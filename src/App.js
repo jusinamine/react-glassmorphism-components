0568311d593d21dce179c76b577864a66599f479
jusinamine <amine.benk27@gmail.com>
2021-04-24 20:20:31 +0100

    glass calculator with multithemes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Calculator from "./components/calculator/Calculator";
+import rosePetals from "./backgrounds/Rose-Petals.svg";
+import liquidCheese from "./backgrounds/Liquid-Cheese.svg";
+import colorfulStingrays from "./backgrounds/Colorful-Stingrays.svg";
+import sunTornado from "./backgrounds/Sun-Tornado.svg";
+
+import { useState } from "react";
 
 function App() {
+  const THEMES = [
+    {
+      svg: rosePetals,
+      color: "#dd1188",
+      calculatorText: "#fff",
+      className: "rose-petals",
+    },
+    {
+      svg: liquidCheese,
+      color: "#FFCC00",
+      calculatorText: "#000",
+      className: "liquid-cheese",
+    },
+    {
+      svg: colorfulStingrays,
+      color: "#037B79",
+      calculatorText: "#000",
+      className: "colorful-stingrays",
+    },
+    {
+      svg: sunTornado,
+      color: "#EE5522",
+      calculatorText: "#000",
+      className: "sun-tornado",
+    },
+  ];
+  const [selectedTheme, setSelectedTheme] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        backgroundImage: `url('${THEMES[selectedTheme].svg}')`,
+        backgroundColor: THEMES[selectedTheme].color,
+        transitionDuration: "1s",
+      }}
+      className="App"
+    >
+      <div
+        className="choose-theme"
+        onClick={() =>
+          setSelectedTheme(
+            selectedTheme + 1 !== THEMES.length ? selectedTheme + 1 : 0
+          )
+        }
+        style={{
+          backgroundImage: `url('${THEMES[selectedTheme].svg}')`,
+          borderColor: THEMES[selectedTheme].color,
+        }}
+      ></div>
+      <Calculator
+        textColor={THEMES[selectedTheme].calculatorText}
+        activeButtonClass={THEMES[selectedTheme].className}
+      />
     </div>
   );
 }
---
+++ src/components/calculator/Calculator.js
@@ -0,0 +1,132 @@
+import React, { useEffect, useRef, useState } from "react";
+import "./calculator.css";
+import VanillaTilt from "vanilla-tilt";
+
+export default function Calculator({ textColor, activeButtonClass }) {
+  const tilt = useRef(null);
+  const [operation, setOperation] = useState({
+    value1: "0",
+    operator: "",
+    value2: "",
+  });
+
+  useEffect(() => {
+    VanillaTilt.init(tilt.current, {
+      speed: 400,
+      max: 15,
+      glare: true,
+      "max-glare": 0.2,
+    });
+  }, []);
+
+  //select operator + - * /
+  const selectOperator = (value) => {
+    if (operation.operator === "")
+      setOperation({ ...operation, operator: value });
+    else if (operation.value2 !== "")
+      setOperation({
+        value1: eval(operation.value1 + operation.operator + operation.value2),
+        operator: "",
+        value2: "",
+      });
+  };
+
+  //select variables of operation
+  const selectVariables = (value) => {
+    if (operation.operator === "")
+      setOperation({
+        ...operation,
+        value1:
+          value === "."
+            ? operation.value1 + value
+            : Number(operation.value1 + value).toString(),
+      });
+    else setOperation({ ...operation, value2: operation.value2 + value });
+  };
+
+  //clear calc input
+  const clearAll = () => {
+    setOperation({ value1: "0", operator: "", value2: "" });
+  };
+
+  //equal result
+  const getResult = () => {
+    if (operation.value2 !== "")
+      setOperation({
+        value1: eval(operation.value1 + operation.operator + operation.value2),
+        operator: "",
+        value2: "",
+      });
+  };
+
+  return (
+    <div ref={tilt} className="container">
+      <div
+        style={{ color: textColor }}
+        className={"calculator " + activeButtonClass}
+      >
+        <input
+          style={{ color: textColor }}
+          type="text"
+          readOnly
+          value={operation.value1 + operation.operator + operation.value2}
+          className="value"
+        />
+        <span className="num clear" onClick={() => clearAll()}>
+          c
+        </span>
+        <span className="num" onClick={() => selectOperator("/")}>
+          /
+        </span>
+        <span className="num" onClick={() => selectOperator("*")}>
+          *
+        </span>
+        <span className="num" onClick={() => selectVariables("7")}>
+          7
+        </span>
+        <span className="num" onClick={() => selectVariables("8")}>
+          8
+        </span>
+        <span className="num" onClick={() => selectVariables("9")}>
+          9
+        </span>
+        <span className="num" onClick={() => selectOperator("-")}>
+          -
+        </span>
+        <span className="num" onClick={() => selectVariables("4")}>
+          4
+        </span>
+        <span className="num" onClick={() => selectVariables("5")}>
+          5
+        </span>
+        <span className="num" onClick={() => selectVariables("6")}>
+          6
+        </span>
+        <span className="num plus" onClick={() => selectOperator("+")}>
+          +
+        </span>
+        <span className="num" onClick={() => selectVariables("1")}>
+          1
+        </span>
+        <span className="num" onClick={() => selectVariables("2")}>
+          2
+        </span>
+        <span className="num" onClick={() => selectVariables("3")}>
+          3
+        </span>
+        <span className="num" onClick={() => selectVariables("0")}>
+          0
+        </span>
+        <span className="num" onClick={() => selectVariables("00")}>
+          00
+        </span>
+        <span className="num" onClick={() => selectVariables(".")}>
+          .
+        </span>
+        <span className="num equal" onClick={() => getResult()}>
+          =
+        </span>
+      </div>
+    </div>
+  );
+}
